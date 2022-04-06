6bb00b9b07bd9d9764aed54ab6c3df66422122e1
mirackarakasli <ilhansamikara@gmail.com>
2022-04-06 23:05:56 +0300

    jwt token

--- blog/models.py
+++ blog/models.py
@@ -1,7 +1,7 @@
-from email.policy import default
 from django.db import models
-from django.contrib.auth.models import User
 from django.utils import timezone
+from django.conf import settings
+
 
 class Category(models.Model):
     name = models.CharField(max_length=100)
@@ -14,24 +14,26 @@ class Post(models.Model):
 
     class PostObjects(models.Manager):
         def get_queryset(self):
-            return super().get_queryset().filter(status='published')
+            return super().get_queryset() .filter(status='published')
 
     options = (
         ('draft', 'Draft'),
         ('published', 'Published'),
     )
 
-
-    category = models.ForeignKey(Category, on_delete=models.PROTECT, default=1)
+    category = models.ForeignKey(
+        Category, on_delete=models.PROTECT, default=1)
     title = models.CharField(max_length=250)
     excerpt = models.TextField(null=True)
     content = models.TextField()
     slug = models.SlugField(max_length=250, unique_for_date='published')
     published = models.DateTimeField(default=timezone.now)
-    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_posts')
-    status = models.CharField(max_length=10, choices=options, default='published')
-    objects = models.Manager() #defaul manager
-    postobjects = PostObjects() # custom manager
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='blog_posts')
+    status = models.CharField(
+        max_length=10, choices=options, default='published')
+    objects = models.Manager()  # default manager
+    postobjects = PostObjects()  # custom manager
 
     class Meta:
         ordering = ('-published',)
